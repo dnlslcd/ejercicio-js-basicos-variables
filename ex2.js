9b876fb0121cfc8ed3f3fb49a2fbe59148429dcb
dnlslcd <danielsalcedoq@gmail.com>
2024-04-16 21:23:40 +0200

    ex2 empezado

--- ex2.js
+++ ex2.js
@@ -7,12 +7,13 @@
  let variable3 = 3;
  let variable4 = 4;
  variable2= 2;
- varialbe3= 4;
+ variable3= 4;
  variable4= 1;
  variable1= -7;
 
  /**
   * En este punto, que valor tendrá 'variable4'. ¿Por qué? ¿Cómo podrías comprobarlo?
+  * para comprobarlo: console.log(variable4)
   */
 
   variable2 = variable1;
@@ -20,4 +21,5 @@
 
 /**
   * En este punto, que valor tendrá 'variable4'. ¿Por qué? ¿Cómo podrías comprobarlo?
-  */
+  * 4
+  */ 
