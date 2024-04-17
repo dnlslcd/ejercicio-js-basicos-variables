a9ad64ac9e6076f403ab994ea43c44ff24724db1
dnlslcd <danielsalcedoq@gmail.com>
2024-04-17 18:07:34 +0200

    ex3 acabado

--- ex3.js
+++ ex3.js
@@ -2,21 +2,21 @@
  * Jugando con string
  */
 
-let nombre = "Mad";
-let apellido = "Max";
+let nombre = "Daniel ";
+let apellido = "Salcedo";
 
-let presentacion = "";
+let presentacion = nombre + apellido;
 
 /**
  * Pregunta 1: ¿Qué valor tengo que asignar a 'presenetacion' para que me muestre pantalla:
  *   * "Me llamo Mad Max?"
- * "nombre + apellido"
+ *"nombre + apellido" (sin las comillas)
  */
 
-console.log("Pregunta 1:", presentacion);
+console.log("Me llamo", presentacion);
 
 /**
  * Pregunta 2: Cambia el valor de las variables adecuadamente para que, en vez de "Me llamo Mad Max", escribe "Me llamo {tu nombre} {tu apellido}"
  */
 
-console.log("Pregunta 2:", presentacion);
+console.log("Estás ante la presencia de", presentacion);
